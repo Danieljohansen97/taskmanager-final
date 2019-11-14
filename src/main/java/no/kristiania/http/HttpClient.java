e10aabe30ae16c8fed4d71223eef0f6817b06b1c
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-11-14 11:59:54 +0100

    HttpClient and HttpClientTest

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,8 +1,33 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
+    private String host;
+    private String requestTarget;
 
+    public HttpClient(String host, String requestTarget) {
+        this.host = host;
+        this.requestTarget = requestTarget;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new HttpClient("urlecho.appspot.com", "/echo?status=200&Content-Type=text%2Fhtml&body=Hello%20world!").executeRequest();
+    }
+
+    HttpClientResponse executeRequest() throws IOException {
+        try (Socket socket = new Socket(host, 80)) {
+            socket.getOutputStream().write(("GET " + requestTarget + " HTTP/1.1\r\n").getBytes());
+            socket.getOutputStream().write(("Host: " + host + "\r\n").getBytes());
+            socket.getOutputStream().write("Connection: close\r\n".getBytes());
+            socket.getOutputStream().write("\n\r".getBytes());
+            socket.getOutputStream().flush();
+
+            HttpClientResponse httpClientResponse = new HttpClientResponse(socket);
+            httpClientResponse.invoke();
+            return httpClientResponse;
+        }
     }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,11 +2,27 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
     @Test
     void mathShouldWork() {
         assertEquals(5, 2+3);
+    }
+
+    @Test
+    void shouldReadStatusCode200() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", "/echo");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void shouldReadStatusCode401() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", "/echo?status=401");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals(401, response.getStatusCode());
     }
 }
